c4b981845d94728aef0c3c11662f4d2e3493e257
DemianUni <damian.rojas.c@uniminuto.edu>
2025-02-26 09:58:58 -0500

    collectStatics

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn task_manager.wsgi --bind 0.0.0.0:$PORT
+web: python manage.py collectstatic && gunicorn task_manager.wsgi --bind 0.0.0.0:$PORT
--- task_manager/settings.py
+++ task_manager/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "whitenoise.runserver_nostatic",
     "django.contrib.postgres",
     # apps propias
     "manager",
@@ -50,6 +51,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     "django.middleware.locale.LocaleMiddleware",
     "django.middleware.security.SecurityMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
@@ -140,6 +142,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = "static/"
+
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
